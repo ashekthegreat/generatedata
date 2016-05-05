984592067626dd7350f415198c40c0a6ee329490
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-05-05 13:20:48 +0600

    Added BabelGeneral data type

---
+++ plugins/dataTypes/BabelGeneral/lang/en.php
@@ -0,0 +1,37 @@
+<?php
+
+$L = array();
+
+$L["DATA_TYPE"] = array(
+    "NAME" => "Babel General",
+    "DESC" => "Generates one or more random values from an arbitrary list of items."
+);
+
+$L["at_most"] = "At Most";
+$L["exactly"] = "Exactly";
+$L["help"] = "Enter a list of items, separated by a pipe | character. Then select whether you want <b>Exactly</b> X number of items, or <b>At most</b> X items from the list. Multiple items are returned in a comma-delimited list in the results. If you want your data set to include empty values, just add one or more pipe characters at the end - the more pipes you enter, the greater the probability of an empty value being generated.";
+$L["separated_by_pipe"] = "Enter values separated by |";
+$L["incomplete_fields"] = "The Custom List Data Type needs to have the format entered in the Options text field. Please fix the following rows:";
+$L["invalid_int_fields"] = "Please only enter numbers in the \"Exactly\" and \"At Most\" fields for the Custom List Data Type. Please fix the following rows:";
+
+/*general*/
+$L["label_1"] = "Coinsurance Maximum Percent";
+$L["value_1"] = "10% Maximum Coinsurance|20% Maximum Coinsurance|30% Maximum Coinsurance|40% Maximum Coinsurance|50% Maximum Coinsurance|75% Maximum Coinsurance|80% Maximum Coinsurance|90% Maximum Coinsurance|Unknown / Not Applicable";
+$L["label_2"] = "Employment Status";
+$L["value_2"] = "Active|Involuntary Leave|Orphan|Pending|Retiree|Unemployed|Unknown";
+$L["label_3"] = "Ethnicity";
+$L["value_3"] = "African|African American|American|American|Asian|Asian Indian|Brazilian|Brazilian|Cambodian|Cape Verdean|Cape Verdean|Caribbean Island|Caribbean Island|Central    American    (not    otherwise specified)|Chinese|Columbian|Cuban|Dominican|Eastern European|Eastern European|European|Filipino|Guatemalan|Haitian|Honduran|Japanese|Korean|Laotian|Mexican, Mexican American, Chicano|Middle Eastern|Other Ethnicity|Other Ethnicity|Portuguese|Portuguese|Puerto Rican|Russian|Russian|Salvadoran|South American (not otherwise specified)|Unknown/Not Specified|Unknown/Not Specified|Vietnamese";
+$L["label_4"] = "Gender";
+$L["value_4"] = "Female|Male|Other|Unknown";
+$L["label_5"] = "Language Preference";
+$L["value_5"] = "English|French|Specified Not Listed|Not Specified";
+$L["label_6"] = "Marital Status";
+$L["value_6"] = "Common Law Married|Divorced|Domestic Partnership|Legally Separated|Married|Never Married|Unknown|Widowed";
+$L["label_7"] = "Metal Level";
+$L["value_7"] = "Bronze|Catastrophic|Gold|Platinum|Silver|Unknown / Not Applicable";
+$L["label_8"] = "Race";
+$L["value_8"] = "American Indian/Alaska Native|American Indian/Alaska Native|Asian|Asian|Black/African American|Black/African American|Native Hawaiian or other Pacific Islander|Native Hawaiian or other Pacific Islander|Other Race|Other Race|Unknown/not specified|Unknown/not specified|White|White";
+$L["label_9"] = "Service Provider Suffix";
+$L["value_9"] = "I.|II.|III.|IV.|V.|Jr.|Sr.|Unknown / Not Applicable";
+
+
