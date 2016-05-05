65ce7e945a56dc6fa9d30fd13e2c2f8d1a58e5f2
Tarafder Ashek E Elahi <mail.ashek@gmail.com>
2016-05-05 13:21:40 +0600

    Added BabelIndicator data type

---
+++ plugins/dataTypes/BabelIndicator/BabelIndicator.class.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * @package DataTypes
+ */
+
+class DataType_BabelIndicator extends DataTypePlugin {
+	protected $isEnabled = true;
+	protected $dataTypeName = "BabelIndicator";
+	protected $dataTypeFieldGroup = "babel";
+	protected $dataTypeFieldGroupOrder = 52;
+	protected $jsModules = array("BabelIndicator.js");
+
+
+	public function generate($generator, $generationContextData) {
+		$options = $generationContextData["generationOptions"];
+		$allElements = explode("|", $options["values"]);
+
+		$val = "";
+		if ($options["listType"] == "Exactly") {
+			$val = implode(", ", Utils::returnRandomSubset($allElements, $options["number"]));
+		} else {
+			// at MOST. So randomly calculate a number up to the num specified
+			$numItems = mt_rand(0, $options["number"]);
+			$val = implode(", ", Utils::returnRandomSubset($allElements, $numItems));
+		}
+
+		return array(
+			"display" => $val
+		);
+	}
+
+	public function getRowGenerationOptionsUI($generator, $postdata, $colNum, $numCols) {
+		if (empty($postdata["dtOption_$colNum"])) {
+			return false;
+		}
+
+		$listType = $postdata["dtListType_$colNum"]; // Exactly or AtMost
+		$number   = ($listType == "Exactly") ? $postdata["dtListExactly_$colNum"] : $postdata["dtListAtMost_$colNum"];
+		$options = array(
+			"listType" => $listType,
+			"number"   => $number,
+			"values"   => $postdata["dtOption_$colNum"]
+		);
+
+		return $options;
+	}
+
+	public function getRowGenerationOptionsAPI($generator, $json, $numCols) {
+		$listType = $json->settings->listType; // Exactly or AtMost
+		$number   = ($listType == "exactly") ? $json->settings->exactly : $json->settings->atMost;
+		$options = array(
+			"listType" => ucfirst($listType),
+			"number"   => $number,
+			"values"   => $json->settings->list
+		);
+
+		return $options;
+	}
+
+	public function getExampleColumnHTML() {
+		$L = Core::$language->getCurrentLanguageStrings();
+		$html =<<< END
+	<select name="dtExample_%ROW%" id="dtExample_%ROW%">
+		<option value="">{$L["please_select"]}</option>
+		<option value="{$this->L["value_1"]}">{$this->L["label_1"]}</option>
+		<option value="{$this->L["value_2"]}">{$this->L["label_2"]}</option>
+		<option value="{$this->L["value_3"]}">{$this->L["label_3"]}</option>
+		<option value="{$this->L["value_4"]}">{$this->L["label_4"]}</option>
+		<option value="{$this->L["value_5"]}">{$this->L["label_5"]}</option>
+		<option value="{$this->L["value_6"]}">{$this->L["label_6"]}</option>
+		<option value="{$this->L["value_7"]}">{$this->L["label_7"]}</option>
+		<option value="{$this->L["value_8"]}">{$this->L["label_8"]}</option>
+		<option value="{$this->L["value_9"]}">{$this->L["label_9"]}</option>
+		<option value="{$this->L["value_10"]}">{$this->L["label_10"]}</option>
+		<option value="{$this->L["value_11"]}">{$this->L["label_11"]}</option>
+		<option value="{$this->L["value_12"]}">{$this->L["label_12"]}</option>
+		<option value="{$this->L["value_13"]}">{$this->L["label_13"]}</option>
+		<option value="{$this->L["value_14"]}">{$this->L["label_14"]}</option>
+		<option value="{$this->L["value_15"]}">{$this->L["label_15"]}</option>
+	</select>
+	<div>
+		&nbsp;{$this->L["separated_by_pipe"]}
+	</div>
+END;
+		return $html;
+	}
+
+	public function getOptionsColumnHTML() {
+		$html =<<< END
+	<div>
+		<input type="radio" name="dtListType_%ROW%" id="dtListType1_%ROW%" value="Exactly" checked="checked"  />
+		<label for="dtListType1_%ROW%">{$this->L["exactly"]}</label>
+		<input type="text" size="2" name="dtListExactly_%ROW%" id="dtListExactly_%ROW%" value="1" />&nbsp;&nbsp;
+		<input type="radio" name="dtListType_%ROW%" id="dtListType2_%ROW%" value="AtMost" />
+		<label for="dtListType2_%ROW%">{$this->L["at_most"]}</label>
+		<input type="text" size="2" name="dtListAtMost_%ROW%" id="dtListAtMost_%ROW%" value="1" />
+	</div>
+	<div>
+		<input type="text" name="dtOption_%ROW%" id="dtOption_%ROW%" style="width: 267px;" />
+	</div>
+END;
+		return $html;
+	}
+
+	public function getDataTypeMetadata() {
+		return array(
+			"SQLField" => "varchar(255) default NULL",
+			"SQLField_Oracle" => "varchar2(255) default NULL",
+			"SQLField_MSSQL" => "VARCHAR(255) NULL"
+		);
+	}
+
+	public function getHelpHTML() {
+		return "<p>{$this->L["help"]}</p>";
+	}
+}
---
+++ plugins/dataTypes/BabelIndicator/lang/en.php
@@ -0,0 +1,49 @@
+<?php
+
+$L = array();
+
+$L["DATA_TYPE"] = array(
+    "NAME" => "Babel Indicator",
+    "DESC" => "Generates one or more random values from an arbitrary list of items."
+);
+
+$L["at_most"] = "At Most";
+$L["exactly"] = "Exactly";
+$L["help"] = "Enter a list of items, separated by a pipe | character. Then select whether you want <b>Exactly</b> X number of items, or <b>At most</b> X items from the list. Multiple items are returned in a comma-delimited list in the results. If you want your data set to include empty values, just add one or more pipe characters at the end - the more pipes you enter, the greater the probability of an empty value being generated.";
+$L["separated_by_pipe"] = "Enter values separated by |";
+$L["incomplete_fields"] = "The Custom List Data Type needs to have the format entered in the Options text field. Please fix the following rows:";
+$L["invalid_int_fields"] = "Please only enter numbers in the \"Exactly\" and \"At Most\" fields for the Custom List Data Type. Please fix the following rows:";
+
+/*Indicators*/
+$L["label_1"] = "Generic";
+$L["value_1"] = "No|Not Applicable|Other|Unknown|Yes";
+$L["label_2"] = "Billable Member";
+$L["value_2"] = "Yes, the member is billable|No, the member is not billable";
+$L["label_3"] = "Capitated Encounter";
+$L["value_3"] = "No|Not Applicable|Other|Unknown|Yes|services are paid under a capitated arrangement|services are not paid under a capitated arrangement|unknown";
+$L["label_4"] = "Compound Drug";
+$L["value_4"] = "Compund drug|No|Non-compound drug|Not Applicable|Other|Unknown|Yes";
+$L["label_5"] = "Health Plan";
+$L["value_5"] = "High deductible|Not high deductible|No|Yes";
+$L["label_6"] = "EPSDT Indicator";
+$L["value_6"] = "EPSDT Referral|EPSDT Screen|EPSDT Treatment|Unknown / Not Applicable / Not Available";
+$L["label_7"] = "Family Planning";
+$L["value_7"] = "Unknown / Not Applicable / Not Avail|Family planning services provided|Abortion services provided|Sterilization services provided|No family planning services provided";
+$L["label_8"] = "Generic Drug";
+$L["value_8"] = "Branded|Generic|No|Not Applicable|Other|Unknown|Yes";
+$L["label_9"] = "Grandfathered Plan";
+$L["value_9"] = "No|Yes|Grandfathered|Non-Grandfathered|Transitional|Not Applicable";
+$L["label_10"] = "InNetwork";
+$L["value_10"] = "1 Participating|2 Non-Participating|3 Unknown/Not Coded|9 No Network for this Plan";
+$L["label_11"] = "Medicare Plan";
+$L["value_11"] = "Advantage|No Medicare Coverage|Not Applicable|Part A and B|Part A Only|Part B Only|Part C Only|Part D Only";
+$L["label_12"] = "Other Insurance Coverage";
+$L["value_12"] = "0 No|1 Yes, other coverage is primary|2 Yes, other coverage is secondary|9 Unknown";
+$L["label_13"] = "Plan Risk";
+$L["value_13"] = "1 Risk (under Maryland contract)|2 Risk (under non-Maryland contract)|3 ASO (employer self-insured)";
+$L["label_14"] = "Single-Multiple Source";
+$L["value_14"] = "Multi-source brand|Multi-source brand with generic equivalent|Single source brand|Single source brand with generic equivalent|Unknown";
+$L["label_15"] = "Special Coverage";
+$L["value_15"] = "Commonwealth Care|Health Safety Net|Not Applicable";
+
+
